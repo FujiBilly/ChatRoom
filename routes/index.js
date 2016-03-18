20912a4799e5f1bfd72be827da90e14b6a9b78be
Billy <chencanbin@szzbmy.com>
2016-03-18 18:10:33 +0800

    fix position

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,17 @@ router.get('/chat', function(req, res, next){
 	res.render('chat', {title: 'NodeJS聊天室'});
 });
 
+router.get('/list', function(req, res, next){
+	var nick = require('../bin/www');
+	var arr = [];
+	for(var i in nick){
+		arr.push(i+','+nick[i]);
+	}
+	res.render('list', {
+		title: "NodeJS聊天室",
+		arr: arr
+	});
+});
+
 
 module.exports = router;
